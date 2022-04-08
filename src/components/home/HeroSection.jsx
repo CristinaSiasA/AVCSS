2601a25bf33f9064fbf6dddc97b8ab83d2dff1ee
CristinaSiasA <x20214006@student.ncirl.ie>
2022-04-08 16:07:00 +0100

    newbutton fixed

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -1,8 +1,7 @@
 import React, {useState} from "react";
-import {Button} from '../common/Button';
 import './heroSection.css'
 import HeroVideo from '../../assets/videos/hero.mp4';
-import BasicSection from "../common/BasicSection";
+import NewButton from "../common/NewButton";
 
 function HeroSection() {
 
@@ -15,23 +14,8 @@ function HeroSection() {
             <h3>It’s Time to Take Control!</h3>
             <h1>Reduce Your <span style={{ color: '#4cb269'}}>Energy</span> Bills <br/> With Clean <span style={{ color: '#68c2c2'}}>Renewable </span>  Energy</h1>
             <div className="hero_buttons">
-               <Button
-              className="buttons"
-              buttonStyle="btn--primary"
-              buttonSize="btn--large"
-              onClick={'/solar'}
-              >
-                  Solar Energy  <i className="fa-regular fa-sun"/>
-              </Button>
-                <Button
-                    className="buttons"
-                    buttonStyle="btn--secondary"
-                    buttonSize="btn--large"
-                    onClick={'/wind'}
-
-                >
-                    Wind Energy <i className="fa-solid fa-wind"/>
-                </Button>
+                <NewButton buttonStyle='my-btn my-btn--primary my-btn--large' url={'/solar'}>Solar Energy <i className="fa-regular fa-sun"/> </NewButton>
+                <NewButton buttonStyle='my-btn my-btn--secondary my-btn--large' url={'/wind'}>Wind Energy <i className="fa-solid fa-wind"/> </NewButton>
             </div>
             </div>
         </div>
