ccf455dfa243126612a0951128d4a00831256455
CristinaSiasA <x20214006@student.ncirl.ie>
2022-04-12 13:07:01 +0100

    remove team section

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -11,7 +11,6 @@ export default function AboutComponents() {
             <AboutTopSection/>
             <MissionSection/>
             <OfferSection/>
-            <TeamSection/>
         </>
     )
 }
