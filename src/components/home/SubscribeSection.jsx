9950b08e787e3a14ab7d8efced9a6f7f967b5fb1
CristinaSiasA <x20214006@student.ncirl.ie>
2022-04-23 00:25:01 +0100

    Message Newsletter Form

--- src/components/home/SubscribeSection.jsx
+++ src/components/home/SubscribeSection.jsx
@@ -1,5 +1,5 @@
-import React from "react";
 import './subscribeSection.css'
+import SubscriptionForm from "./SubscriptionForm";
 
 
 function SubscribeSection (){
@@ -9,20 +9,10 @@ function SubscribeSection (){
     <h1 className='subscription_header display-5'>
         Subscribe to Our Newsletter
     </h1>
-    <h3 className='style-subtitles'>
+    <h3 className='style-subtitles opacity-50'>
        You Can Unsubscribe Any Time
     </h3>
-    <div className='input_area'>
-        <form>
-            <input
-                className='subscription_input text-muted'
-                name='email'
-                type='email'
-                placeholder='Enter Your Email'
-            />
-            <button type="submit" className="btn btn-outline-light btn-lg" >Subscribe</button>
-        </form>
-    </div>
+    <SubscriptionForm/>
 </section>
     )
 }
