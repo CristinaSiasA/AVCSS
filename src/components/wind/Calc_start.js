d3ff5f65b19d7cd0b7ee7a89569821f3ccbb8946
Daniel O'Driscoll <daodrisc@tcd.ie>
2022-04-23 15:09:25 +0100

    test commit

--- src/components/wind/Calc_start.js
+++ src/components/wind/Calc_start.js
@@ -7,10 +7,7 @@ const Calc_start = ({ goToCalc }) => {
       <div className="calculator-container">
         <div className="img-div">
           <div className="description-position">
-            <h3 className="calculator-description">
-              {" "}
-              Find out how much you can save with our wind energy calculator!
-            </h3>
+            <h3 className="calculator-description"> TEST!</h3>
 
             <button
               className="goto_calc"
